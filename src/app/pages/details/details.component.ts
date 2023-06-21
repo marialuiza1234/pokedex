bf73212e86ede992ec2f34f6b8bf5a3da93b4499
Maria Luiza <50643764+marialuiza1234@users.noreply.github.com>
2023-06-21 10:47:29 -0300

    Update details.component.ts

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -27,7 +27,7 @@ export class DetailsComponent implements OnInit {
     this.getPokemon;
   }
 
-  public getPokemon(){
+  get getPokemon(){
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
